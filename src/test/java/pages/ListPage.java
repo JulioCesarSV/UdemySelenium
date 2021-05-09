426c83c5acef253b00da58eb1dc6d791c8f63c48
JulioCesarSV <JulioCesarSV@users.noreply.github.com>
2021-05-09 14:23:15 -0500

    Add new Page to test List

--- src/test/java/pages/ListPage.java
+++ src/test/java/pages/ListPage.java
@@ -7,11 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListPage extends BasePage{
-    private String searchField = "";
+    private String searchField = "//body/form[1]/input[1]";
     private String searchResults = "name";
 
     public ListPage() {
         super(driver);
+    }
+
+    public void navigateToListPage(){
+        navigateTo("https://andreidbr.github.io/JS30/06AjaxTypeAhead/index.html");
     }
 
     public void enterSearchCriteria() throws InterruptedException {
--- src/test/java/steps/SandBoxSteps.java
+++ src/test/java/steps/SandBoxSteps.java
@@ -3,17 +3,31 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.ListPage;
+
+import java.util.List;
 
 public class SandBoxSteps {
+    ListPage listPage = new ListPage();
     @Given("I navigate to the list page")
     public void iNavigateToTheListPage() {
+    listPage.navigateToListPage();
     }
 
     @When("I search the list")
-    public void iSearchTheList() {
+    public void iSearchTheList() throws InterruptedException {
+        listPage.enterSearchCriteria();
     }
 
     @Then("I can find the text in the list")
     public void iCanFindTheTextInTheList() {
+        List<String> lista = listPage.getAllSearchResults();
+        boolean textIsThere = lista.contains("Seattle, Washington");
+
+        if (textIsThere){
+            System.out.println("The text in on the list");
+        }else {
+            throw new Error("No text");
+        }
     }
 }
